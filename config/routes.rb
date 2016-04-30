e93f9dbca1cedc4b9cd1a2bdc91edc30bc637d70
star__hoshi <kensuke1751@gmail.com>
2016-04-30 21:37:57 +0900

    success twitter login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get "home/index" => 'home/index'
+  get '/auth/:provider/callback', :to => 'sessions#callback'
+  post '/auth/:provider/callback', :to => 'sessions#callback'
+  get '/logout' => 'sessions#destroy', :as => :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
